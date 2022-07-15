e60a22caa74abe08f4e6213007c3f85d50af637e
germione <germione.grangere@gmail.com>
2022-07-15 12:18:03 +0200

    feat(users): register endpoint

--- src/plugins/users.ts
+++ src/plugins/users.ts
@@ -1,5 +1,12 @@
 import Hapi from "@hapi/hapi";
+import Boom from "@hapi/boom";
 import Joi from "joi";
+import bcrypt from "bcrypt";
+import jwt, { SignOptions } from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "SUPER_SECRET_JWT_SECRET";
+const JWT_ALGORITHM = "HS256";
+const AUTHENTICATION_TOKEN_EXPIRATION_HOURS = 1;
 
 interface UserPayload {
   user: {
@@ -42,7 +49,64 @@ async function registerUserHandler(
   request: Hapi.Request,
   h: Hapi.ResponseToolkit
 ) {
-  return h.response({ users: null }).code(200);
+  const { prisma } = request.server.app;
+  const {
+    user: { email, username, password },
+  } = request.payload as UserPayload;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      throw Boom.badRequest("user already exists");
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const createdUser = await prisma.user.create({
+      data: {
+        email,
+        username,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        bio: true,
+        image: true,
+      },
+    });
+
+    const token = generateJwtToken(createdUser.id);
+
+    const response = {
+      email: createdUser.email,
+      username: createdUser.username,
+      bio: createdUser.bio,
+      image: createdUser.image,
+      token,
+    };
+    return h.response({ users: response }).code(200);
+  } catch (err: any) {
+    request.log("error", err);
+    return Boom.badImplementation("failed to register user");
+  }
+}
+
+// Generate a signed JWT token with the tokenId in the payload
+function generateJwtToken(tokenId: number): string {
+  const jwtPayload = { tokenId };
+  const signInOptions: SignOptions = {
+    algorithm: JWT_ALGORITHM,
+    expiresIn: AUTHENTICATION_TOKEN_EXPIRATION_HOURS,
+    noTimestamp: true,
+  };
+  return jwt.sign(jwtPayload, JWT_SECRET, signInOptions);
 }
 
 export default usersPlugin;
