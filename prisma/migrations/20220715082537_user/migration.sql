74a23c15be83c980117845c680e8aa5ffba0994b
germione <germione.grangere@gmail.com>
2022-07-15 10:33:15 +0200

    feat(db): add user model

---
+++ prisma/migrations/20220715082537_user/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT,
+    "password" TEXT NOT NULL,
+    "bio" TEXT DEFAULT '',
+    "image" TEXT DEFAULT '',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
