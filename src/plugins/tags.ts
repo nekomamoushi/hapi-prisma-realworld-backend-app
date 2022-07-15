c5bbfdde7c9111373fbefcd71b178ef4a821e0ce
germione <germione.grangere@gmail.com>
2022-07-15 10:22:38 +0200

    feat(tags): get all tags endpoint

--- src/plugins/tags.ts
+++ src/plugins/tags.ts
@@ -1,4 +1,5 @@
 import Hapi from "@hapi/hapi";
+import Boom from "@hapi/boom";
 
 const tagsPlugin: Hapi.Plugin<any> = {
   name: "tags",
@@ -6,10 +7,22 @@ const tagsPlugin: Hapi.Plugin<any> = {
     server.route({
       method: "GET",
       path: "/tags",
-      handler: (_, h: Hapi.ResponseToolkit) =>
-        h.response({ tags: [] }).code(200),
+      handler: getTagsHandler,
     });
   },
 };
+
+async function getTagsHandler(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+  const { prisma } = request.server.app;
+
+  try {
+    const tags = await prisma.tag.findMany({ select: { tag: true } });
+    const response = tags.map((t) => t.tag);
+    return h.response({ tags: response }).code(200);
+  } catch (err: any) {
+    console.error(err.message);
+    return Boom.badImplementation("failed to get tags");
+  }
+}
 
 export default tagsPlugin;
