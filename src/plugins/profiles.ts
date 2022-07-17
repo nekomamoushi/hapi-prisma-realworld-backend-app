a2f162a3eb65bca6b9a63e9b570ddc0792d1cb99
germione <germione.grangere@gmail.com>
2022-07-17 14:18:09 +0200

    feat(profiles): get user profile endpoint

--- src/plugins/profiles.ts
+++ src/plugins/profiles.ts
@@ -1,10 +1,15 @@
 import Hapi from "@hapi/hapi";
+import Boom from "@hapi/boom";
 
 const profilesPlugin: Hapi.Plugin<any> = {
   name: "profiles",
   dependencies: ["prisma"],
   register: async function (server: Hapi.Server) {
-    console.log("register profiles plugin");
+    await server.route({
+      method: "GET",
+      path: "/profiles/{username}",
+      handler: getProfileHandler,
+    });
   },
 };
 
@@ -12,8 +17,30 @@ async function getProfileHandler(
   request: Hapi.Request,
   h: Hapi.ResponseToolkit
 ) {
+  const { prisma } = request.server.app;
   const { username } = request.params;
-  console.log(username);
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      throw Boom.notFound(`coul not find user: ${username}`);
+    }
+    const response = {
+      username: user.username,
+      bio: user.bio,
+      image: user.image,
+      following: false,
+    };
+    return h.response({ profiles: response }).code(200);
+  } catch (err: any) {
+    request.log("error", err);
+    return Boom.badImplementation("failed to get profile");
+  }
 }
 
 export default profilesPlugin;
