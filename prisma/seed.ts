9d6ef8ea1019374d0dfb337870c5cedc33636b8b
germione <germione.grangere@gmail.com>
2022-07-23 10:32:17 +0200

    update seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,50 +5,79 @@ const prisma = new PrismaClient();
 
 async function main() {
   await prisma.tag.deleteMany();
+  await prisma.comment.deleteMany();
+  await prisma.article.deleteMany();
   await prisma.user.deleteMany();
 
-  const tags = await prisma.tag.createMany({
-    data: [
-      {
-        tag: "angular",
-      },
-      { tag: "javascript" },
-    ],
-  });
+  await prisma.$queryRaw`ALTER SEQUENCE "Tag_id_seq" RESTART WITH 1`;
+  await prisma.$queryRaw`ALTER SEQUENCE "Comment_id_seq" RESTART WITH 1`;
+  await prisma.$queryRaw`ALTER SEQUENCE "Article_id_seq" RESTART WITH 1`;
+  await prisma.$queryRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1`;
 
-  const tagCount = await prisma.tag.aggregate({
-    _count: true,
-  });
-
-  const users = await prisma.user.createMany({
-    data: [
-      {
-        username: "germione",
-        email: "germione@prisma.com",
-        password: bcrypt.hashSync("passeword", 10),
-      },
-      {
-        username: "jayee",
-        email: "jayee@prisma.com",
-        password: bcrypt.hashSync("passeword", 10),
-      },
-      {
-        username: "naboo",
-        email: "naboo@prisma.com",
-        password: bcrypt.hashSync("passeword", 10),
-      },
-    ],
-  });
-
-  const user = await prisma.user.findUnique({
-    where: {
+  const user1 = await prisma.user.create({
+    data: {
+      username: "germione",
       email: "germione@prisma.com",
+      password: bcrypt.hashSync("passeword", 10),
+      articles: {
+        createMany: {
+          data: [
+            {
+              title: "How to defy your cat",
+              slug: "how-to-defy-your-cat",
+              description: "Hard right?",
+              body: "meow strongly",
+              tagList: ["defy", "cats"],
+            },
+            {
+              title: "How to eat a fish",
+              slug: "how-to-eat-a-fish",
+              description: "Easy Peasy",
+              body: "open your mouth strongly",
+              tagList: ["eat", "fish"],
+            },
+          ],
+        },
+      },
+    },
+  });
+
+  const user2 = await prisma.user.create({
+    data: {
+      username: "jayee",
+      email: "jayee@prisma.com",
+      password: bcrypt.hashSync("passeword", 10),
+    },
+  });
+
+  const user3 = await prisma.user.create({
+    data: {
+      username: "naboo",
+      email: "naboo@prisma.com",
+      password: bcrypt.hashSync("passeword", 10),
+      articles: {
+        createMany: {
+          data: [
+            {
+              title: "How Angular Standalone components works",
+              slug: "how-angular-standalone-components-works",
+              description: "Simplify angular development",
+              body: "We don't use NgModules anymore",
+              tagList: ["angular", "modules", "standalone", "components"],
+            },
+          ],
+        },
+      },
     },
   });
 
   const userCount = await prisma.user.aggregate({
     _count: true,
   });
+  console.log(`${userCount._count} users created:`);
+  console.log(`  - ${user1.username}`);
+  console.log(`  - ${user2.username}`);
+  console.log(`  - ${user3.username}`);
 }
 
 main()
